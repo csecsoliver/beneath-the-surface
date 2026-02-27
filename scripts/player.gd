7755a26aad45a391deb57597e04e6d36ada66451
csecsoliver <0oliogamer0@gmail.com>
2026-02-28 00:51:19 +0100

    player movement and animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 
 const SPEED = 10.0
-const MAX_SPEED = 200.0
+const MAX_SPEED = 200.0 # sideways. double of terminal velocity
 const JUMP_VELOCITY = -150.0
 const GRAVITY = 300.0
 
 func _physics_process(delta: float) -> void:
+	
+	if abs(velocity.y) < 20:
+		%PlayerSprite.animation = "idle"
+		
+	
 	# handle gravity with low terminal velocity
 	if not is_on_floor() and velocity.y < MAX_SPEED*0.5:
 		velocity.y += GRAVITY * delta
@@ -16,8 +21,17 @@ func _physics_process(delta: float) -> void:
 	if direction and abs(velocity.x) < MAX_SPEED:
 		velocity.x += SPEED * direction
 	else:
+		# friction sideways
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction:
+		%PlayerSprite.animation = "swim"
 	
+	if Input.is_action_pressed("move_down") and velocity.y < MAX_SPEED: # increase descent speed over terminal velocity
+		velocity.y += SPEED
+	elif velocity.y > MAX_SPEED*0.5: # slow down descent to terminal velocity
+		velocity.y = move_toward(velocity.y, MAX_SPEED*0.5, SPEED)
+	if Input.is_action_pressed("move_down"):
+		%PlayerSprite.animation = "down"
 	# Handle jump.
 	if Input.is_action_just_pressed("move_up"):
 		velocity.y = JUMP_VELOCITY
@@ -32,8 +46,18 @@ func _physics_process(delta: float) -> void:
 			if normal.x > 0 and Input.is_action_just_pressed("move_kick"):
 				velocity.x += MAX_SPEED*2
 			elif Input.is_action_just_pressed("move_kick"):
-				velocity.x += MAX_SPEED*2
-				
+				velocity.x -= MAX_SPEED*2
+	
+	if velocity.y > 20:
+		%PlayerSprite.animation = "down"
+	if velocity.y < -20:
+		%PlayerSprite.animation = "up"
+	if direction > 0:
+		%PlayerSprite.flip_h = true
+	elif direction < 0:
+		%PlayerSprite.flip_h = false
+	
+	
 	
 	move_and_slide()
 	

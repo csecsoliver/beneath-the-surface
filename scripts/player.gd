23650d32142f3f56033f5d26389575326caf44a8
csecsoliver <0oliogamer0@gmail.com>
2026-02-27 22:56:40 +0100

    player script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,41 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 10.0
+const MAX_SPEED = 200.0
+const JUMP_VELOCITY = -150.0
+const GRAVITY = 300.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# handle gravity with low terminal velocity
+	if not is_on_floor() and velocity.y < MAX_SPEED*0.5:
+		velocity.y += GRAVITY * delta
+		
+	# handle movement
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction and abs(velocity.x) < MAX_SPEED:
+		velocity.x += SPEED * direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("move_up"):
+		velocity.y = JUMP_VELOCITY
+		# make it go faster if jumping
+		if direction and abs(velocity.x) < MAX_SPEED * 3:
+			velocity.x += SPEED*5 * direction
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var normal = collision.get_normal()
+		if abs(normal.x) > 0.5:
+			if normal.x > 0 and Input.is_action_just_pressed("move_kick"):
+				velocity.x += MAX_SPEED*2
+			elif Input.is_action_just_pressed("move_kick"):
+				velocity.x += MAX_SPEED*2
+				
+	
 	move_and_slide()
+	
+	
+	

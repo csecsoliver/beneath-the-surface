fd2d263199c47a9b059476eda3aed414718738f1
penzboti <penzboti@gmail.com>
2026-02-28 07:45:31 +0100

    trident collision

--- scripts/trident.gd
+++ scripts/trident.gd
@@ -2,12 +2,34 @@ extends Node2D
 
 var direction: Vector2
 @export var speed: int = 100
+@export var invincibility_timer: float = 0.1
+@export var vanish_timer: float = 9.99
+
+var timer: float = 0
+
+@onready var char: CharacterBody2D = $CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$CharacterBody2D/CollisionShape2D.disabled = true
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += direction.normalized() * delta * speed
+	
+	timer += delta
+	if timer > invincibility_timer: $CharacterBody2D/CollisionShape2D.disabled = false
+	if timer > vanish_timer: self.queue_free()
+	
+	char.velocity = direction.normalized() * speed
+	char.move_and_slide()
+
+func _physics_process(_delta: float) -> void:
+	for i in char.get_slide_collision_count():
+		var collision = char.get_slide_collision(i)
+		if collision.get_collider().get_parent().has_meta("shark"):
+			print("shark")
+			collision.get_collider().get_parent().queue_free()
+		if collision.get_collider() is TileMapLayer:
+			self.queue_free()
